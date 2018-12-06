05223b67789439f51a85336caf20a14f57118114
lively-teacher-9822 <ccrive10@gmail.com>
2018-12-06 20:26:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-function takeANumber(currentLine, name){
-  currentLine.push(name);
-  return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
+ var number = 1;
+
+function takeANumber(){
+  return number + 1;
+  // representing the # of people in the current line
 }
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
   } else {
+    // I know that shift removes the first element of an array, we want it to remove it and return it.
    return "Currently serving " + katzDeliLine.shift()+'.';  
   }
 }
+
+// rewrite the take a number function so that instead of returning 
 
 function currentLine (line){
   if (line.length === 0){
@@ -17,7 +22,7 @@ function currentLine (line){
   } else {
     var sentence = "The line is currently:";
     for(var i = 0; i<line.length; i++){
-      
+          //looking to check that we're not at the last element, if we are not at the last element, do not add the commas, if we are, add the commas.
       if (i !== line.length - 1) {
         sentence = `${sentence} ${i + 1}. ${line[i]},`;
        } else {
